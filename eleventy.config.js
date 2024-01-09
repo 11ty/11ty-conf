cc9da90da1b0afc7dafdd48d75ce1fe912231ab3
Zach Leatherman <zachleat@users.noreply.github.com>
2024-01-09 10:58:55 -0600

    Image optimization for footer image

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,16 @@
 import pluginWebc from "@11ty/eleventy-plugin-webc";
+import { eleventyImagePlugin } from "@11ty/eleventy-img";
+
 
 export default function(eleventyConfig) {
 	eleventyConfig.ignores.add("README.md");
 
-	eleventyConfig.addPlugin(pluginWebc);
+	eleventyConfig.addPlugin(pluginWebc, {
+		components: [
+			"_components/**/*.webc",
+			"npm:@11ty/eleventy-img/*.webc",
+		]
+	});
 
 	eleventyConfig.addWatchTarget("_components/**/*.css");
 
@@ -11,4 +18,16 @@ export default function(eleventyConfig) {
 		"public/*": "/public/",
 		"node_modules/@11ty/logo/assets/logo-bg.svg": "/public/logo.svg",
 	}).addPassthroughCopy("robots.txt");
+
+	// Image plugin
+	eleventyConfig.addPlugin(eleventyImagePlugin, {
+		// Set global default options
+		formats: ["avif", "webp"],
+		urlPath: "/public/built/",
+
+		defaultAttributes: {
+			loading: "lazy",
+			decoding: "async"
+		}
+	});
 };
